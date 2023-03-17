70b00212dda7aa57751287e3bd9222ac72848fda
Daniil Litvinenko <danil.fire@bk.ru>
2023-03-17 23:07:03 +0700

    Now you can enter a name

---
+++ Lesson_UI_/Assets/Scripts/EnterName.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EnterName : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private TMP_InputField _inputField;
+
+    public void SetName()
+    {
+        _text.text = _inputField.text;
+        _inputField.text = "";
+    }
+}
